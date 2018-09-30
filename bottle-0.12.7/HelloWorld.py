b35e71fdd9f707cfeb46f4ad786f6e256460af6b
Mark Perera <marksachin12@gmail.com>
2018-09-30 19:08:25 -0400

    so the backend is moving along gr8. data is extracted from the user and the search key is displayed on the back page. i need to deal with separating the words, probably use a hash table(array). then send that to the front end to be displayed

--- bottle-0.12.7/HelloWorld.py
+++ bottle-0.12.7/HelloWorld.py
@@ -1,5 +1,16 @@
-from bottle import route, run
+from bottle import route, run, template, request, static_file
+
+
 @route('/')
-def hello():
-	return "Hello World"
+def welcome_page():
+    return template('indexSearchPage')
+    #the web famework will launch the above page as the intro page
+
+
+
+@route('/results', method='POST')
+def returnResults():
+	searchKey = request.forms.get('searchKey')
+	return template('searchResultPage.html', searchResult = searchKey)
+
 run(host='localhost', port=8080, debug=True)
