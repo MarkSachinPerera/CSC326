a5063ff8b84896e2129f9272ea2ed37badd5d423
MarkSachinPerera <marksachin12@gmail.com>
2018-09-30 20:18:36 -0400

    The backend is complete, still need to test and adjust for corner cases

--- bottle-0.12.7/HelloWorld.py
+++ bottle-0.12.7/HelloWorld.py
@@ -1,5 +1,5 @@
 from bottle import route, run, template, request, static_file
-
+from collections import Counter
 
 @route('/')
 def welcome_page():
@@ -10,7 +10,16 @@ def welcome_page():
 
 @route('/results', method='POST')
 def returnResults():
+	#Take the key word 
+	#split it
+	#count repeats 
 	searchKey = request.forms.get('searchKey')
-	return template('searchResultPage.html', searchResult = searchKey)
+	splitKey = searchKey.split(" ")
+	keyWordCount = {i:splitKey.count(i) for i in splitKey}
+
+	#return 2 values the whole search and the split
+	#return template('{{key}}', key = keyWordCount)
+	return template('searchResultPage.html', searchResult = searchKey,
+		displayArray = keyWordCount)
 
 run(host='localhost', port=8080, debug=True)
