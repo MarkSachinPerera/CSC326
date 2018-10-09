206019f75b9036a0771d2c8619cd5cb6cf73455b
tavallat <tina.tava@outlook.com>
2018-10-09 18:27:23 -0400

    commented front end for lab1

--- bottle-0.12.7/HelloWorld.py
+++ bottle-0.12.7/HelloWorld.py
@@ -4,6 +4,7 @@ from heapq import nlargest
 
 
 allinputs = dict()
+#this dict holds every single input ever entered in on session
 
 @route('/')
 def welcome_page():
@@ -14,6 +15,7 @@ def welcome_page():
 @route('/static/<filename>')
 def server_static(filename):
     return static_file (filename, root='./myfiles')
+#this is to specify the picture directory for indexSearchPage
 
 
 @route('/results', method='POST')
@@ -32,11 +34,14 @@ def returnResults():
 
 
         notshared = dict()
+        #this dict holds every word that was not shared between current input and allinputs
         shared = dict()
+        #this dict holds every word that was shared between current input and allinputs
 
+        #accounting for if the input dict is empty so i dont reach segfault
         if len(allinputs) == 0:
             allinputs.update(keyWordCount)
-
+#finding whats shared and not shared
         else:
             for key in keyWordCount:
                 if key in allinputs:
@@ -45,18 +50,18 @@ def returnResults():
                 else:
                     temp2 = {key: keyWordCount[key]}
                     notshared.update(temp2)
-
+#updating allinputs with new arrays
         allinputs.update(shared)
         allinputs.update(notshared)
 #by now I have stored every word ever entered and the number of times
 #it was repeated over a session in allinputs
-
+#t20 holds the top 20 most shared by finding the biggest valued keys
         t20 = dict()
         counter = 0
         tempp = dict()
         tempp.update(allinputs)
 
-
+#accounting for segfaults again
         if len(allinputs) < 20:
             t20.update(allinputs)
         else:
