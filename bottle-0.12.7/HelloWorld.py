a2482e98861d07a8e77460673bab014111c7bf4f
MarkSachinPerera <MarkSachinPerera@github.com>
2018-12-03 11:11:48 -0500

    lab 4 done

--- bottle-0.12.7/HelloWorld.py
+++ bottle-0.12.7/HelloWorld.py
@@ -1,3 +1,4 @@
+import json
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.client import flow_from_clientsecrets
 #from googleapiclient.errors import HttpError
@@ -10,28 +11,22 @@ import bottle
 from bottle import error
 import searchDB
 allinputs = dict()
-import json
 usermanager = dict()
 ar = dict()
 s = dict()
 searchedword = " "
 
 
-
-# urls = {
-#     #rank / url
-#     1: "http://www.google.com",
-#     3: "http://www.yahoo.com",
-#     6: "http://www.facebook.com",
-#     2: "http://www.twitter.com",
-#     5: "http://www.instagram.com",
-#     4: "http://www.linkedin.com",
-#     7: "http://www.snapchat.com",
-#     10: "http://wwww.github.com",
-#     8: "http://www.outlook.com",
-#     11: "http://www.q.utoronto.ca",
-#     9 : "http://www.youtube.com"
-# }
+funurls = {
+    #rank / url
+    1: "http://www.zoomquilt.org",
+    2: "http://www.nooooooooooooooo.com",
+    3: "https://cat-bounce.com",
+    4: "https://www.pointerpointer.com",
+    5: "https://www.flightradar24.com",
+    6: "http://www.snapbubbles.com",
+    7: "http://www.sanger.dk",
+}
 
 
 # session setting
@@ -44,27 +39,25 @@ session_opts = {
 app = SessionMiddleware(bottle.app(), session_opts)
 
 
-
-
 @error(404)
 def error404(error):
     return 'Nothing here, sorry'
 
 
-
 @route('/')
 def welcome_page():
     return template('ask.html')
-    #the web famework will launch the above page as the intro page
+    # the web famework will launch the above page as the intro page
 
-#Lab 3 addition
+# Lab 3 addition
+
+
 @route('/nextpage', method='POST')
 def nextpage():
     global s
     # s holds all the ranks:urls in increasing order of their ranking
 
     # i only want to hold the top 5 for each result
-
 
     if len(s) <= 5:
         global s
@@ -86,12 +79,19 @@ def nextpage():
                     displayArray=ar, sortedurl=top5)
 
 
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except:
+        return False
+
 
 @route('/aresults', method='POST')
 def areturnResults():
-    # Take the key word
-    # split it
-    # count repeats
+        # Take the key word
+        # split it
+        # count repeats
     global searchKey
     searchKey = request.forms.get('searchKey')
     searchKey = searchKey.lower()
@@ -99,22 +99,62 @@ def areturnResults():
     global searchedword
     searchedword = searchKey.split(' ', 1)[0]
     keyWordCount = {i: splitKey.count(i) for i in splitKey}
+    s1 = searchKey.split(' ', 1)[0]
+    if s1 == "tark":
+        searchKey = searchKey.split(' ', 1)[1]
+        s2 = searchKey.split(' ', 1)[0]
+        if s2 == "im" or s2 == "i'm":
+            searchKey = searchKey.split(' ', 1)[1]
+            s3 = searchKey.split(' ', 1)[0]
+            if s3 == 'bored':
+                return template('fun.html', funlist=funurls)
 
+    sign = " "
+    final = 100000000000000000000000000
+    searchedword1 = searchKey.split(' ', 1)[0]
+    if is_float(searchedword1):
+        firstnum = float(searchedword1)
+        searchKey = searchKey.split(' ', 1)[1]
+        searchedword2 = searchKey.split(' ', 1)[0]
+        if searchedword2 == '+' or searchedword2 == '-' or searchedword2 == '/' or searchedword2 == '*' or searchedword2 == 'plus' or searchedword2 == 'minus' or searchedword2 == 'divide' or searchedword2 == 'times':
+            sign = searchedword2
+            searchKey = searchKey.split(' ', 1)[1]
+            searchedword3 = searchKey.split(' ', 1)[0]
+            if is_float(searchedword3):
+                secondnum = float(searchedword3)
+                if sign == "+" or sign == "plus":
+                    final = firstnum + secondnum
+                if sign == "-"or sign == "minus":
+                    final = firstnum - secondnum
+                if sign == "/" or sign == "divide":
+                    final = firstnum / secondnum
+                if sign == "*" or sign == "times":
+                    final = firstnum * secondnum
+                helllo = {1: final}
+                searched = str(firstnum) + sign + str(secondnum)
+                return template('anonsearchResultPage.html', searchResult=searched,
+                                displayArray=helllo, sortedurl=helllo)
+            else:
+                pass
+        else:
+            pass
+
+    else:
+        pass
     print searchedword
     urls = searchDB.generate_search_results(searchedword)
 
-
-    #LAB 3 : Assuming that the dict with urls and their rank is sent to me here :
-    #sort the dictionary, assuming that the dict is called urls
+    # LAB 3 : Assuming that the dict with urls and their rank is sent to me here :
+    # sort the dictionary, assuming that the dict is called urls
     if len(urls) == 0:
         return template('ask.html')
 
     for key in urls.iterkeys():
         global s
         s[key] = urls[key]
-    #s holds all the ranks:urls in increasing order of their ranking
+    # s holds all the ranks:urls in increasing order of their ranking
 
-    #i only want to hold the top 5 for each result
+    # i only want to hold the top 5 for each result
 
     if len(s) <= 5:
         top5 = s
@@ -128,22 +168,19 @@ def areturnResults():
             del s[wantedkey]
             counter = counter + 1
 
-
-
     global searched
     searched = searchKey
     global ar
     ar = keyWordCount
 
-
     return template('anonsearchResultPage.html', searchResult=searched,
-                    displayArray=keyWordCount, sortedurl = top5)
-
+                    displayArray=keyWordCount, sortedurl=top5)
 
 
 @route('/signedin', method='GET')
 def home():
-    flow = flow_from_clientsecrets("client_secrets.json", scope='https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email', redirect_uri="http://localhost:8080/redirect")
+    flow = flow_from_clientsecrets(
+        "client_secrets.json", scope='https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email', redirect_uri="http://localhost:8080/redirect")
 
     uri = flow.step1_get_authorize_url()
     bottle.redirect(str(uri))
@@ -163,10 +200,9 @@ def redirect_page():
     users_service = build('oauth2', 'v2', http=http)
     user_document = users_service.userinfo().get().execute()
     session['email'] = user_document['email']
-    #if they have a name registered, I also hold their name
+    # if they have a name registered, I also hold their name
     if 'name' in user_document:
         session['name'] = user_document['name']
-
 
     user_email = user_document['email']
     global email
@@ -175,21 +211,19 @@ def redirect_page():
     return template('indexSearchPage.html', EMAIL=session['name'])
 
 
-
-#this dict holds every single input ever entered in on session
-
-
+# this dict holds every single input ever entered in on session
 
 
 @route('/static/<filename>')
 def server_static(filename):
-    return static_file (filename, root='./myfiles')
-#this is to specify the picture directory for indexSearchPage
+    return static_file(filename, root='./myfiles')
+# this is to specify the picture directory for indexSearchPage
+
 
 @route('/signout')
 def sign_out():
     session = request.environ.get('beaker.session')
-    eemail = session.setdefault('email','')
+    eemail = session.setdefault('email', '')
     global usermanager
     global allinputs
 
@@ -200,18 +234,9 @@ def sign_out():
     bottle.redirect('/')
 
 
-
-@route('/anon', method = 'GET')
+@route('/anon', method='GET')
 def ahome():
     return template('anonindexSearchPage.html')
-
-
-
-
-
-
-
-
 
 
 @route('/results', method='POST')
@@ -222,32 +247,31 @@ def returnResults():
     except:
         email = ''
 
-	#Take the key word
-	#split it
-	#count repeats
-	searchKey = request.forms.get('searchKey')
+        # Take the key word
+        # split it
+        # count repeats
+        searchKey = request.forms.get('searchKey')
         searchKey = searchKey.lower()
-	splitKey = searchKey.split(" ")
-	keyWordCount = {i:splitKey.count(i) for i in splitKey}
+        splitKey = searchKey.split(" ")
+        keyWordCount = {i: splitKey.count(i) for i in splitKey}
         global allinputs
         eemail = session.setdefault('email', '')
 
         if eemail in usermanager:
             allinputs = usermanager[eemail]
 
-	#return 2 values the whole search and the split
-	#return template('{{key}}', key = keyWordCount)
-
+        # return 2 values the whole search and the split
+        # return template('{{key}}', key = keyWordCount)
 
         notshared = dict()
-        #this dict holds every word that was not shared between current input and allinputs
+        # this dict holds every word that was not shared between current input and allinputs
         shared = dict()
-        #this dict holds every word that was shared between current input and allinputs
+        # this dict holds every word that was shared between current input and allinputs
 
-        #accounting for if the input dict is empty so i dont reach segfault
+        # accounting for if the input dict is empty so i dont reach segfault
         if len(allinputs) == 0:
             allinputs.update(keyWordCount)
-#finding whats shared and not shared
+# finding whats shared and not shared
         else:
             for key in keyWordCount:
                 if key in allinputs:
@@ -256,37 +280,36 @@ def returnResults():
                 else:
                     temp2 = {key: keyWordCount[key]}
                     notshared.update(temp2)
-#updating allinputs with new arrays
+# updating allinputs with new arrays
         allinputs.update(shared)
         allinputs.update(notshared)
-#by now I have stored every word ever entered and the number of times
-#it was repeated over a session in allinputs
-#t20 holds the top 20 most shared by finding the biggest valued keys
+# by now I have stored every word ever entered and the number of times
+# it was repeated over a session in allinputs
+# t20 holds the top 20 most shared by finding the biggest valued keys
         t20 = dict()
         counter = 0
         tempp = dict()
         tempp.update(allinputs)
 
-#accounting for segfaults again
+# accounting for segfaults again
         if len(allinputs) < 20:
             t20.update(allinputs)
         else:
             t20 = dict(Counter(tempp).most_common(20))
 
 
-#t20 holds the overall top 20 most searched stuff
-        eemail = session.setdefault('email','')
-        name = session.setdefault('name','')
+# t20 holds the overall top 20 most searched stuff
+        eemail = session.setdefault('email', '')
+        name = session.setdefault('name', '')
         # global email
         # myemail = email
         # print(eemail)
         global usermanager
 
-
         usermanager[eemail] = t20
 
+        return template('searchResultPage.html', searchResult=searchKey,
+                        displayArray=keyWordCount, top20=t20, EMAIL=name)
 
-        return template('searchResultPage.html', searchResult = searchKey,
-                    displayArray = keyWordCount, top20=t20, EMAIL=name)
 
-run(app = app, host='0.0.0.0', port=80)
+run(app=app, host='0.0.0.0', port=80)
